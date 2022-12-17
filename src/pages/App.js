b3f4cc279f3d6303a2d106c1f069c0c0cde2272f
Vinícius Santos da Silva <vinicius.2101.santos@gmail.com>
2022-12-17 16:31:05 -0300

    Dando funcionalidade ao botão de remover

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,9 +34,8 @@ function App() {
   }
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
+    const newReposPage = repos.filter(repo => repo.id !== id)
+    setRepos(newReposPage)
   }
 
 
